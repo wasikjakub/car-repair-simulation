a97ecd4617197286c377c799cc0ba9a7c5fe71af
Jakub Wąsik <144064944+wasikjakub@users.noreply.github.com>
2024-10-15 17:52:36 +0200

    skeleton

---
+++ main.py
@@ -0,0 +1,9 @@
+class Workshop:
+    def __init__(self, num_of_mechanics):
+        self.mechanics = num_of_mechanics
+
+    def enque(slef):
+        return 0
+    
+    def deque(self):
+        return 0
